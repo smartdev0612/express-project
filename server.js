87fd7c05ea6300e2f7ec67c4b8e19bf926675d59
Smart Dev <danieldev0612@gmail.com>
2023-03-13 20:54:26 -0700

    setup MVC patter

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 const express = require('express')
 
+const friendsController = require('./controllers/friends.controller')
+const messagesController = require('./controllers/messages.controller')
+
 const app = express()
 
 const PORT = 3000
-
-const friends = [
-  {
-    id: 0,
-    name: 'Albert Einstein',
-  },
-  {
-    id: 1,
-    name: 'Sir Isaac Newton',
-  },
-]
 
 app.use((req, res, next) => {
   const start = Date.now()
@@ -24,44 +16,12 @@ app.use((req, res, next) => {
 
 app.use(express.json())
 
-app.post('/friends', (req, res) => {
-  if (!req.body.name) {
-    return res.status(400).json({
-      error: 'Missing friend name',
-    })
-  }
-  const newFriend = {
-    name: req.body.name,
-    id: friends.length,
-  }
-  friends.push(newFriend)
-  res.json(newFriend)
-})
+app.post('/friends', friendsController.postFriend)
+app.get('/friends', friendsController.getFriends)
+app.get('/friends/:friendId', friendsController.getFriend)
 
-app.get('/friends', (req, res) => {
-  res.json(friends)
-})
-
-// /friends/22
-app.get('/friends/:friendId', (req, res) => {
-  const friendId = Number(req.params.friendId)
-  const friend = friends[friendId]
-  if (friend) {
-    res.status(200).json(friend)
-  } else {
-    res.status(404).json({
-      error: 'Friend does not exist',
-    })
-  }
-})
-
-app.get('/messages', (req, res) => {
-  res.send('<ul><li>Hello Albert!</li></ul>')
-})
-
-app.post('/messages', (req, res) => {
-  console.log('Updating messages...')
-})
+app.get('/messages', messagesController.getMessages)
+app.post('/messages', messagesController.postMessage)
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`)
